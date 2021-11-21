10d1515231c599e5e4290d085810b15b3459a666
kirtibaghel <94801589+kirtibaghel@users.noreply.github.com>
2021-11-22 00:44:35 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -7,10 +7,35 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
+#include <conio.h>
 
-int main()
+void main()
 {
-    printf("Hello World");
-printf()
-    return 0;
+int a=10,b=20,c=30;
+if (a>b)
+  {if (a>c)
+    printf("a is largest");
+    else
+    printf("b is largest");
+  }
+else
+{ if (b>c)
+    printf("b is largest");
+else
+    printf("c is largst");
 }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
