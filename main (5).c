7717788f8075cff18be7ff4086549e851b5560fb
kirtibaghel <94801589+kirtibaghel@users.noreply.github.com>
2021-12-15 12:22:18 +0530

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ char name[10]="delhi";
+ printf("city %s",name);
+ printf("in enter your name");
+ scanf("%s",name);
+ printf("your name is %s",name);
+} 
