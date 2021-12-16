4750af19007798cda9d45f4f64fc8033745d7d72
kirtibaghel <94801589+kirtibaghel@users.noreply.github.com>
2021-12-16 22:01:12 +0530

    Add files via upload

---
+++ main (11).c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <string.h>
+#include <stdio.h>
+
+void main()
+{
+ int diff;
+ diff=strcmp("A","B");
+ printf("difference of A and B %d ",diff);
+
+}    
+    
+    
+    
+    
+ 
+    
+    
+    
+    
