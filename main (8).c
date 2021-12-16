b14e4752c5f3fd5acbc7b4c341343da7d5b59ef4
kirtibaghel <94801589+kirtibaghel@users.noreply.github.com>
2021-12-16 21:49:29 +0530

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <string.h>
+#include <stdio.h>
+
+void main()
+{
+    char name[10];
+    //name "delhi";
+    strcpy(name,"delhi");
+    printf("name %s",name);
+}    
+    
+    
+    
+    
