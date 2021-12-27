d44a9491eec2f357ea3e2865604e3bfa373daaa1
kirtibaghel <94801589+kirtibaghel@users.noreply.github.com>
2021-12-27 19:26:39 +0530

    Add files via upload

---
+++ main (14).c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <conio.h>
+#include <stdio.h>
+
+void prime(int num);
+void main()
+
+{
+ int num;
+ clrscr();
+ printf("enter any number");
+ scanf("%d"&num);
+ prime(num);
+}
+
+void prime(int num)
+{
+    int i,flag=1,n;
+    
+    for (n=2;n<=num;n++)
+    {
+        flag=1;
+        for(i=2;i<n;i++)
+        {
+        if(n%i==0)
+         {
+             flag=0;
+             break;
+         }
+        }
+        if(flag==1)
+        printf("\n%d prime number",n);
+    }
+}
+         
